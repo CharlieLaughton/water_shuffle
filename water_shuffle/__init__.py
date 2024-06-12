ebd5b4f2cad4c66feab3561c03f20526e839d6ae
Charlie Laughton <charles.laughton@nottingam.ac.uk>
2024-06-12 16:26:17 +0100

    Updated tests

--- water_shuffle/__init__.py
+++ water_shuffle/__init__.py
@@ -4,14 +4,7 @@ from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 from argparse import ArgumentParser
 
-# trajfile = sys.argv[1]
-# topfile = sys.argv[2]
-# outfile = sys.argv[3]
-# water_resname = sys.argv[4]
-# n_cycles = int(sys.argv[5])
-
-
-def run():
+def cli():
     parser = ArgumentParser()
     parser.add_argument('--trajin', help='input trajectory')
     parser.add_argument('--topology', help='topology file')
@@ -22,30 +15,48 @@ def run():
     args = parser.parse_args()
 
     t = mdt.load(args.trajin, top=args.topology)
-    w = t.topology.select(f'resname {args.water_name} and mass > 2')
+    trajout = run(t,
+        args.water_name,
+        args.n_cycles,
+        cli_run=True)
+
+    trajout.save(args.trajout)
+
+def run(t, water_name, n_cycles, cli_run=False):
+    w = t.topology.select(f'resname {water_name} and mass > 2')
     n_w = len(w)
     if n_w == 0:
-        print(f'Error: no water molecules with residue name {args.water_name}')
-        exit(1)
+        if cli_run:
+            print(f'Error: no water molecules with residue name {water_name}')
+            exit(1)
+        else:
+            raise ValueError(f'Error: no water molecules with residue name {water_name}')
     dw = w[1:] - w[:-1]
     if not np.all(dw == dw[0]):
-        print('Error - waters must be contiguous in the file')
-        exit(1)
+        if cli_run:
+            print('Error - waters must be contiguous in the file')
+            exit(1)
+        else:
+            raise ValueError('Error - waters must be contiguous in the file')
+            
     n_aw = dw[0]
     if n_aw > 1:
         ro = t.topology.atom(w[0]).residue.index
         for j in range(1, n_aw):
             rh = t.topology.atom(w[0]+j).residue.index
             if rh != ro:
-                print('Error - waters must have their O atoms listed first')
-                exit(1)
+                if cli_run:
+                    print('Error - waters must have their O atoms listed first')
+                    exit(1)
+                else:
+                    raise ValueError('Error - waters must have their O atoms listed first')
     w_start = w[0]
     w_end = w[-1] + n_aw
     tw = t.atom_slice(w)
     x_opt = tw.xyz.copy()
     x_avg = x_opt[0]
     w_ind = np.zeros(n_w * n_aw, dtype=int)
-    for i in range(args.n_cycles):
+    for i in range(n_cycles):
         cost = 0.0
         for j, x in enumerate(x_opt):
             dm = cdist(x_avg, x_opt[j])
@@ -56,10 +67,11 @@ def run():
             for k in range(1, n_aw):
                 w_ind[k::n_aw] = w_ind[::n_aw] + k
             t.xyz[j, w_start:w_end] = t.xyz[j, w_ind]
-        print(f'cycle {i}: mean cost: {cost/len(x_opt):8.2f}')
+        if cli_run:
+            print(f'cycle {i}: mean cost: {cost/len(x_opt):8.2f}')
         x_avg = x_opt.mean(axis=0)
-    t.save(args.trajout)
+    return t
 
 
 if __name__ == '__main__':
-    run()
+    cli()
